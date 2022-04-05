b5f34674028db30e14ece8ccba3f8c6f0e044911
Andreas Wänqvist <andreas.wanqvist@voyado.com>
2022-04-05 17:54:11 +0200

    slots, composition and portals

---
+++ templating/src/8-dynamic-composition/composition-page.ts
@@ -0,0 +1,5 @@
+export class CompositionPage {
+    public myObj = { name: 'Voyado' };
+    public view = require("./view.html");
+    public viewFromModel = '<div>Hello World, from the model...</div>';
+}
